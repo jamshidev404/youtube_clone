1f30f40d409aec63da3c2507ed4a506d0b38ee13
jamshidev404 <jamshidbek536@gmail.com>
2023-03-15 15:05:14 +0500

    fetchAPI

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,15 @@
 import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
-import Sidebar from "./Sidebar";
+import { Sidebar, Videos } from "./index";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippets&q=${selectedCategory}`);
+  }, []);
+  
   return (
     <>
       <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
@@ -17,10 +24,24 @@ const Feed = () => {
           <Typography
             className='coppyright'
             variant='body2'
-            sx={{ mt: 1.5, color: "#fff" }}
+            sx={{ mt: 0.6, color: "#fff" }}
           >
             All Rights Reserved
           </Typography>
+        </Box>
+
+        <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+          <Typography
+            variant='h4'
+            fontWeight='bold'
+            mb={2}
+            sx={{ color: "white" }}
+          >
+            New
+            <span style={{ color: "#f31503" }}> videos</span>
+          </Typography>
+
+          <Videos videos={[]} />
         </Box>
       </Stack>
     </>
